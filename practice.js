2e480c556fd9c9ecb25a35837455ee3b319e3755
rishabhsmallo21 <95697367+rishabhsmallo21@users.noreply.github.com>
2023-01-24 09:30:05 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,20 @@
+// let fruits=["apple"];
+// fruits.push("banana","orange");
+// fruits.unshift("strawberry","kiwi");
+// alert(fruits);
+// console.log(fruits);
+
+// let arr=["I","study","JavaScript"];
+// arr.splice(1,1);
+// alert(arr);
+// arr.splice(2,0,"complex","language");
+// alert(arr);
+// let arr1=["I","study","JavaScript","right","now"];
+// arr1.splice(0,3,"Let's","dance");
+// alert(arr1);
+
+
+// let arr2=["I","study","JavaScript"];
+// arr2.splice(2,0,"complex","language");
+// alert(arr2);
+
